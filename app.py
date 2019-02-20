5e250a18dcf04ad31fdf92a7048dbc465b951b25
Venus Chau <vc2000@wildcats.unh.edu>
2019-02-20 21:26:18 +0800

    modified app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Flask Dockerized'
+    return "Flask Dockerized"
 
-@app.route('/pig')
+
+@app.route("/pig")
 def huy():
-    return 'huy_pig'
+    return "huy_pig"
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
